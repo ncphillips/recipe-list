050504bbacfa49a58c7216003cbd9c0528e95850
nolan <nolan@ncphi.com>
2023-11-28 18:17:00 -0400

    when viewing a grocery list we can see it's items + quantity

--- app/controllers/grocery_lists_controller.rb
+++ app/controllers/grocery_lists_controller.rb
@@ -1,5 +1,16 @@
 class GroceryListsController < ApplicationController
+  before_action :set_grocery_list, only: [:show]
+
   def index
     @grocery_lists = GroceryList.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_grocery_list
+    @grocery_list = GroceryList.find(params[:id])
   end
 end
--- test/system/grocery_lists_test.rb
+++ test/system/grocery_lists_test.rb
@@ -2,7 +2,8 @@ require "application_system_test_case"
 
 class GroceryListsTest < ApplicationSystemTestCase
   setup do
-    @recipe = GroceryList.create(name: "Test Recipe")
+    @list = GroceryList.create(name: "Test Recipe")
+    @list.add(1.cup, ingredients(:peanut_butter))
   end
 
   test "visiting the index" do
@@ -10,5 +11,12 @@ class GroceryListsTest < ApplicationSystemTestCase
 
     assert_selector "h1", text: "Grocery Lists"
     assert_link "Test Recipe"
+  end
+
+  test "viewing a grocery list" do
+    visit grocery_list_url(@list)
+
+    assert_selector "h1", text: "Test Recipe"
+    assert_selector "li", text: "1 cup of Peanut Butter"
   end
 end
